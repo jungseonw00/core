21767cc2104da35e8435bd08da941debc536b91e
seonwoojung <laborlawseon@gmail.com>
2022-11-02 11:06:56 +0900

    - Bean 등록 - 자동 빈 등록 - Lombok 사용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,11 +7,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // @Component 어노테이션이 붙은 것들을 Bean으로 등록 해준다.
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 
 public class AutoAppConfig {
-
 }
