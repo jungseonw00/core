d4aaff7e9f883b0cbd75f2d263728366554a19fa
seonwoojung <laborlawseon@gmail.com>
2022-11-05 20:08:09 +0900

    - Bean 만들기

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -14,7 +14,17 @@ public class PrototypeTest {
     @Test
     void prototypeBeanFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
 
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        ac.close();
     }
 
     @Scope("prototype")
